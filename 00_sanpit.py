edaf0d85fe98122973c8d213c46dfedf25123759
itztasha <78763122+itztasha@users.noreply.github.com>
2021-02-24 09:17:00 +1300

    Update 00_sanpit.py

--- 00_sanpit.py
+++ 00_sanpit.py
@@ -1,8 +1,5 @@
-# Ask users for a number
-get_number = int(input("Choose a number: "))
+#ask users for number
 
-# multiply by 5
-get_number = get_number * 5
 
-# Output the resulst
-print(get_number)
+
+
