f9d639cb3ec7e03196e123159834f680e641317d
itztasha <78763122+itztasha@users.noreply.github.com>
2021-02-25 14:19:43 +1300

    Update 00_sanpit.py

--- 00_sanpit.py
+++ 00_sanpit.py
@@ -1,5 +1,9 @@
-#ask users for number
+# ask users for number
+get_number = int(input("choose a number "))
 
+# multiply the number by 5
+times_five= get_number * 5
 
-
-
+# output the results
+print ("{} times five is equal to "
+       "{}".format (get_number,times_five))
